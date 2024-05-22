68f39867e1f7b1b1f1fd48f1320a5459ca74ec27
stanleyyuen <102275989+stanleyyconsensys@users.noreply.github.com>
2024-05-22 15:20:36 +0800

    feat: add bufferToString

--- packages/snap/src/modules/bitcoin/wallet/account.ts
+++ packages/snap/src/modules/bitcoin/wallet/account.ts
@@ -62,19 +62,11 @@ export class BtcAccount implements IBtcAccount {
     if (!this.#payment) {
       this.#payment = getBtcPaymentInst(
         this.scriptType,
-        this.pubkeyToBuf(this.pubkey),
+        hexToBuffer(this.pubkey),
         this.network,
       );
     }
     return this.#payment;
-  }
-
-  protected pubkeyToBuf(pubkey: string): Buffer {
-    try {
-      return hexToBuffer(pubkey, false);
-    } catch (error) {
-      throw new Error('Public key is invalid');
-    }
   }
 }
 
