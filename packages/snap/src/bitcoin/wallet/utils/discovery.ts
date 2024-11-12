9ab45a7fdf2a27cf789986f7aa0a13c8851eacb7
gantunesr <17601467+gantunesr@users.noreply.github.com>
2024-11-12 17:00:23 -0300

    chore: remove binarySearch method

--- packages/snap/src/bitcoin/wallet/utils/discovery.ts
+++ packages/snap/src/bitcoin/wallet/utils/discovery.ts
@@ -26,61 +26,11 @@ export const hasActivity = async (
 };
 
 /**
- * Perform a binary search to find the set of used and unused addresses.
+ * Linear search for the first address with activity.
  * @param scope - The CAIP-2 Chain ID.
- * @param addresses - Array of addresses to check.
+ * @param addresses - Address list to check for activity.
  * @returns Promise that resolves into an object containing the used and unused addresses.
  */
-export const binarySearch = async (
-  scope: string,
-  addresses: string[],
-): Promise<{ unusedAddresses: string[]; usedAddresses: string[] }> => {
-  if (addresses.length === 0) {
-    return { unusedAddresses: [], usedAddresses: [] };
-  }
-
-  if (addresses.length === 1) {
-    const hasAct = await hasActivity(scope, addresses[0]);
-    return hasAct
-      ? { unusedAddresses: [], usedAddresses: [addresses[0]] }
-      : { unusedAddresses: [addresses[0]], usedAddresses: [] };
-  }
-
-  const middleIndex = Math.floor(addresses.length / 2);
-  const middleElement = addresses[middleIndex];
-  const leftArray = addresses.slice(0, middleIndex);
-  const rightArray = addresses.slice(middleIndex + 1);
-  const { unusedAddresses: unusedRight, usedAddresses: usedRight } =
-    await binarySearch(scope, rightArray);
-
-  if (usedRight.length > 0) {
-    return {
-      unusedAddresses: unusedRight,
-      usedAddresses: [...leftArray, middleElement, ...usedRight],
-    };
-  }
-
-  if (await hasActivity(scope, addresses[middleIndex])) {
-    return {
-      usedAddresses: [...leftArray, middleElement, ...usedRight],
-      unusedAddresses: unusedRight,
-    };
-  }
-
-  // Check the index here
-  const { unusedAddresses: unusedLeft, usedAddresses: usedLeft } =
-    await binarySearch(scope, leftArray);
-
-  if (usedLeft.length > 0) {
-    return {
-      unusedAddresses: [...unusedLeft, addresses[middleIndex], ...unusedRight],
-      usedAddresses: [...usedLeft],
-    };
-  }
-
-  return { unusedAddresses: addresses, usedAddresses: [] };
-};
-
 export const linearSearch = async (
   scope: string,
   addresses: string[],
