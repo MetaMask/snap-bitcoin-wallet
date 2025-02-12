10e963a892fa3942afea21d15db36a27893ac62a
Dario Anongba Varela <dario.anongba@gmail.com>
2025-02-12 19:37:47 +0100

    add old files back to move

---
+++ packages/snap/src/entities/send-form.ts
@@ -0,0 +1,98 @@
+import type { CurrencyRate } from '@metamask/snaps-sdk';
+import type { Network } from 'bitcoindevkit';
+
+import type { CurrencyUnit } from './currency';
+
+export const SENDFORM_NAME = 'sendForm';
+
+export type SendFormContext = {
+  account: string;
+  network: Network;
+  balance: string;
+  feeRate: number;
+  currency: CurrencyUnit;
+  fiatRate?: CurrencyRate;
+  recipient?: string;
+  amount?: string;
+  fee?: string;
+  drain?: boolean;
+  errors: {
+    tx?: string;
+    recipient?: string;
+    amount?: string;
+  };
+};
+
+export enum SendFormEvent {
+  Amount = 'amount',
+  Recipient = 'recipient',
+  ClearRecipient = 'clearRecipient',
+  Confirm = 'confirm',
+  Cancel = 'cancel',
+  SetMax = 'max',
+}
+
+export type SendFormState = {
+  recipient: string;
+  amount: string;
+};
+
+export type ReviewTransactionContext = {
+  feeRate: number;
+  currency: CurrencyUnit;
+  fiatRate?: CurrencyRate;
+  recipient: string;
+  amount: string;
+  fee: string;
+
+  /**
+   * Used to repopulate the send form if the user decides to go back in the flow
+   * Optional when sending directly without form.
+   */
+  sendForm?: SendFormContext;
+};
+
+export enum ReviewTransactionEvent {
+  Send = 'send',
+  HeaderBack = 'headerBack',
+}
+
+/**
+ * SendFlowRepository is a repository that manages Bitcoin Send flow interfaces.
+ */
+export type SendFlowRepository = {
+  /**
+   * Get the form state.
+   * @param id - the interface ID
+   * @returns the form state
+   */
+  getState(id: string): Promise<SendFormState>;
+
+  /**
+   * Insert a new send form interface.
+   * @param context - the form context
+   * @returns the interface ID
+   */
+  insertForm(context: SendFormContext): Promise<string>;
+
+  /**
+   * Update an interface to the send form view.
+   * @param id - the interface ID
+   * @param context - the form context
+   */
+  updateForm(id: string, context: SendFormContext): Promise<void>;
+
+  /**
+   * Insert a new review transaction interface.
+   * @param id - the interface ID
+   * @param context - the review transaction context
+   */
+  insertReview(context: ReviewTransactionContext): Promise<string>;
+
+  /**
+   * Update an interface to the review transaction view.
+   * @param id - the interface ID
+   * @param context - the review transaction context
+   */
+  updateReview(id: string, context: ReviewTransactionContext): Promise<void>;
+};
