f3d4a79986cc4a4b80f41c213d1193a04db4be83
Dario Anongba Varela <dario.anongba@gmail.com>
2025-02-21 13:19:32 +0100

    done

--- packages/snap/src/handlers/AssetsHandler.test.ts
+++ packages/snap/src/handlers/AssetsHandler.test.ts
@@ -11,17 +11,17 @@ describe('AssetsHandler', () => {
   describe('lookup', () => {
     it('returns data for all networks', () => {
       const result = handler.lookup();
+      console.log(result);
+
       expect(result.assets[Caip19Asset.Bitcoin]?.name).toEqual('Bitcoin');
-      expect(result.assets[Caip19Asset.Bitcoin]?.name).toEqual(
+      expect(result.assets[Caip19Asset.Testnet]?.name).toEqual(
         'Testnet Bitcoin',
       );
-      expect(result.assets[Caip19Asset.Bitcoin]?.name).toEqual(
+      expect(result.assets[Caip19Asset.Testnet4]?.name).toEqual(
         'Testnet4 Bitcoin',
       );
-      expect(result.assets[Caip19Asset.Bitcoin]?.name).toEqual(
-        'Signet Bitcoin',
-      );
-      expect(result.assets[Caip19Asset.Bitcoin]?.name).toEqual(
+      expect(result.assets[Caip19Asset.Signet]?.name).toEqual('Signet Bitcoin');
+      expect(result.assets[Caip19Asset.Regtest]?.name).toEqual(
         'Regtest Bitcoin',
       );
     });
@@ -30,7 +30,7 @@ describe('AssetsHandler', () => {
   describe('conversion', () => {
     it('returns empty record', () => {
       const result = handler.conversion();
-      expect(result).toEqual({});
+      expect(result.conversionRates).toEqual({});
     });
   });
 });
