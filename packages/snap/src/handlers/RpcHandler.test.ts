ba804b14b3a36b6138efd658ba9e49a1618f3fc5
Dario Anongba Varela <dario.anongba@gmail.com>
2025-05-19 17:11:06 +0200

    done

--- packages/snap/src/handlers/RpcHandler.test.ts
+++ packages/snap/src/handlers/RpcHandler.test.ts
@@ -72,6 +72,7 @@ describe('RpcHandler', () => {
 
     it('propagates errors from display', async () => {
       const error = new Error();
+      mockAccountsUseCases.sendPsbt.mockReset();
       mockSendFlowUseCases.display.mockRejectedValue(error);
 
       await expect(handler.route(origin, mockRequest)).rejects.toThrow(
