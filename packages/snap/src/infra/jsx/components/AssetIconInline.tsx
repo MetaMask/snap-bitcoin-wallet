59769932eeebb19ec7dd8502db14279eb56fef71
orestis <orestis.foufris@consensys.net>
2025-10-09 13:51:50 +0100

    fix: add smaller btc logo image

--- packages/snap/src/infra/jsx/components/AssetIconInline.tsx
+++ packages/snap/src/infra/jsx/components/AssetIconInline.tsx
@@ -2,12 +2,12 @@ import type { Network } from '@metamask/bitcoindevkit';
 import type { SnapComponent } from '@metamask/snaps-sdk/jsx';
 import { Image } from '@metamask/snaps-sdk/jsx';
 
-import btcIcon from '../images/bitcoin.svg';
+import btcIconInline from '../images/bitcoin-inline.svg';
 import signetIcon from '../images/signet.svg';
 import testnetIcon from '../images/testnet.svg';
 
 const networkToIcon: Record<Network, string> = {
-  bitcoin: btcIcon,
+  bitcoin: btcIconInline,
   testnet: testnetIcon,
   testnet4: testnetIcon,
   signet: signetIcon,
