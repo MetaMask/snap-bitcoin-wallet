1463842e87f08cc16e778447e1f689a59824f611
orestis <orestis.foufris@consensys.net>
2025-10-29 15:28:52 +0000

    Revert "feat: sync account if `synchronize` flag is true for existing accounts"
    This reverts commit c7d2b3ac88511dc8fc81ce8a2c4bddacbaa46ef9.

--- packages/snap/src/use-cases/AccountUseCases.ts
+++ packages/snap/src/use-cases/AccountUseCases.ts
@@ -147,30 +147,25 @@ export class AccountUseCases {
     ];
 
     // Idempotent account creation + ensures only one account per derivation path
-    let account = await this.#repository.getByDerivationPath(derivationPath);
+    const account = await this.#repository.getByDerivationPath(derivationPath);
     if (account && account.network === network) {
       this.#logger.debug('Account already exists: %s,', account.id);
-    } else {
-      account = await this.#repository.create(
-        derivationPath,
-        network,
-        addressType,
-      );
-
-      account.revealNextAddress();
-
-      await this.#repository.insert(account);
-
-      this.#logger.info(
-        'Bitcoin account created successfully: %s. Public address: %s, Request: %o',
-        account.id,
-        account.publicAddress,
-        req,
-      );
+      return account;
     }
 
+    const newAccount = await this.#repository.create(
+      derivationPath,
+      network,
+      addressType,
+    );
+
+    newAccount.revealNextAddress();
+
+    await this.#repository.insert(newAccount);
+
+    // First notify the event has been created, then schedule full scan.
     await this.#snapClient.emitAccountCreatedEvent(
-      account,
+      newAccount,
       correlationId,
       accountName,
     );
@@ -179,11 +174,17 @@ export class AccountUseCases {
       await this.#snapClient.scheduleBackgroundEvent({
         duration: 'PT1S',
         method: CronMethod.FullScanAccount,
-        params: { accountId: account.id },
+        params: { accountId: newAccount.id },
       });
     }
 
-    return account;
+    this.#logger.info(
+      'Bitcoin account created successfully: %s. Public address: %s, Request: %o',
+      newAccount.id,
+      newAccount.publicAddress,
+      req,
+    );
+    return newAccount;
   }
 
   async synchronize(account: BitcoinAccount, origin: string): Promise<void> {
