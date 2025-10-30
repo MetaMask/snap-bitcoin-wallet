1e17f4efdb665aac576e703775c37d2485c97878
Charly Chevalier <charly.chevalier@consensys.net>
2025-10-30 12:29:28 +0100

    chore: lint

--- packages/snap/src/store/BdkAccountRepository.ts
+++ packages/snap/src/store/BdkAccountRepository.ts
@@ -119,9 +119,9 @@ export class BdkAccountRepository implements BitcoinAccountRepository {
     // READ THIS CAREFULLY:
     // The BDK expects 4-bytes fingerprint, so we have to pad with 0, in case
     // our fingerprint contains leading null-bytes.
-    const fingerprint = (
-      slip10.masterFingerprint ?? slip10.parentFingerprint
-    ).toString(16).padStart(8, '0');
+    const fingerprint = (slip10.masterFingerprint ?? slip10.parentFingerprint)
+      .toString(16)
+      .padStart(8, '0');
 
     const xpub = slip10_to_extended(slip10, network);
     const descriptors = xpub_to_descriptor(
